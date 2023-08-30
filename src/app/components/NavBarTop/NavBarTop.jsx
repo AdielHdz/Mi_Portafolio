93fd326f696117eb6ce1a08b6ae36a904d5acd3e
Adiel <wereber.dragons14@live.com.mx>
2023-08-29 22:17:19 -0600

    diseño

--- src/app/components/NavBarTop/NavBarTop.jsx
+++ src/app/components/NavBarTop/NavBarTop.jsx
@@ -1,10 +1,35 @@
+"use client";
 import Link from "next/link";
 import { CiMenuKebab } from "react-icons/ci";
 const NavBarTop = () => {
+  const handlerTheme = (e) => {
+    const value = e.target.checked;
+    if (value) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  };
+
   return (
-    <nav className="sticky top-0 h-16 flex items-center justify-between p-3 bg-veryDarkGray text-whites">
+    <nav className="sticky z-20 top-0 h-16 flex items-center justify-between p-3 bg-blue_gray dark:bg-blacks dark:shadow-little_dark text-whites">
       <h3 className="text-lg">{"{ AH }"}</h3>
-      <input type="checkbox" id="menu" className="hidden peer" />
+      <label
+        className="relative w-12 h-6  bg-whites peer-checked:bg-rose-400 rounded-full"
+        htmlFor="theme"
+      >
+        <input
+          type="checkbox"
+          id="theme"
+          className="peer hidden"
+          onChange={handlerTheme}
+        />
+        <div
+          htmlFor="theme"
+          className="absolute shadow-little bg-gray-500 w-5 h-5 rounded-full top-0.5 left-0.5 transform transition-all duration-200 peer-checked:translate-x-6 peer-checked:bg-blacks"
+        ></div>
+      </label>
+      {/*   <input type="checkbox" id="menu" className="hidden peer" />
       <label
         htmlFor="menu"
         className="transition-colors duration-300 h-10 w-10 rounded-full cursor-pointer hover:bg-whites flex items-center justify-center hover:text-veryDarkGray md:hidden"
@@ -12,18 +37,18 @@ const NavBarTop = () => {
         <CiMenuKebab className="w-8 h-8 transform rotate-90" />
       </label>
 
-      <menu className="fixed top-0 right-0 md:inline-block md:relative md:h-auto md:w-auto md:opacity-100 md:transform-none bg-veryDarkGray opacity-0 h-screen w-full transform translate-x-full -translate-y-full transition-all peer-checked:opacity-100 duration-300 peer-checked:-translate-x-0 peer-checked:translate-y-0 flex justify-center items-center shadow-lg">
+      <section className="fixed  top-0 right-0 md:inline-block md:relative md:h-auto md:w-auto md:opacity-100 md:transform-none bg-veryDarkGray opacity-0 h-screen w-full transform translate-x-full -translate-y-full transition-all peer-checked:opacity-100 duration-300 peer-checked:-translate-x-0 peer-checked:translate-y-0 flex justify-center items-center shadow-lg">
         <label
           htmlFor="menu"
           className="absolute top-2 right-2 md:hidden w-8 h-8  flex items-center justify-center"
         >
-          <div className="absolute w-5 h-0 border border-blue_icons bg-blue_icons rotate-45"></div>
-          <div className="absolute w-5 h-0 border border-blue_icons bg-blue_icons -rotate-45"></div>
+          <div className="absolute w-5 h-0 border border-teal-500 bg-teal-500 rotate-45"></div>
+          <div className="absolute w-5 h-0 border border-teal-500 bg-teal-500 -rotate-45"></div>
         </label>
         <Link className="text-whites text-lg" href="#">
           About
         </Link>
-      </menu>
+      </section> */}
     </nav>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,16 +9,24 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        primary: ["Itim", "cursive"],
-        secondary: ["Jaldi"],
+        itim: ["Itim", "cursive"],
+        jaldi: ["Jaldi", "sans-serif"],
+        dongle: ["Dongle", "sans-serif"],
       },
       colors: {
         whites: "#ffffff",
+        blacks: "#000000",
         veryDarkGray: "#282828",
         blue_icons: "#39B4CF",
+        blue_gray: "#4E6E81",
       },
       width: {
         noventaCinco: "95%",
+      },
+      boxShadow: {
+        little: "0 0 2px 1px rgba(0,0,0,0.5)",
+        little_dark: "0 0 2px 1px rgba(255,255,255,0.5)",
+        little_in: "inset 0 0 2px 1px rgba(0,0,0,0.5)",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
